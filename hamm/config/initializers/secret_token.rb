79430e05d5306193c7e2972dff65238302457c28
anthony <anthony@anthony-VirtualBox.(none)>
2014-09-03 18:41:10 -0600

    creación de rails app

---
+++ hamm/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hamm::Application.config.secret_key_base = 'ac28e8710bff968340d52807a2bd32d86c59ccf588c10c79609317b136dde8496ac4eb636dba836edc629ecad2909bf5986d1d67842bf419cf10ac1f2f2874ce'
---
+++ hamm/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hamm::Application.config.session_store :cookie_store, key: '_hamm_session'
